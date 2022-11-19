dee2f3ddcf8aecaf378d6494821c2dffacb39479
Katidian <catherinedarden@gmail.com>
2022-11-18 22:14:54 -0500

    Add script with multiple linear regression

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+mecha_table <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+head(mecha_table)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table))
