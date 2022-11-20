69d0f988feee43c03c7c2ed1468bbf50ac6d51a0
Katidian <catherinedarden@gmail.com>
2022-11-20 12:08:46 -0500

    Add summary tables for PSI from suspension coil data

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,15 @@ library(dplyr)
 
 mecha_table <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 
+sc_table <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
 head(mecha_table)
 
+head(sc_table)
+
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table))
+
+total_summary <- sc_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary <- sc_table %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
